cf435269e622c675da1b078f17affa74ae92d4a4
王文国 <924958592@qq.com>
2021-06-13 19:47:33 +0800

    完成用户列表功能

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
-/* eslint-disable vue/no-unused-components */
 <template>
   <div id="app">
     <!-- 路由占位符 -->
-   <router-view></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-// import Login from '@/components/Login'
 export default {
   name: 'app'
-  // components: {
-  //   // eslint-disable-next-line vue/no-unused-components
-  //   Login
-  // }
 }
 </script>
 
 <style>
-
-</style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -26,6 +26,7 @@
     </div>
   </div>
 </template>
+
 <script>
 export default {
   data () {
